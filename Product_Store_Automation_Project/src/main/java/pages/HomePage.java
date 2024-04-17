50674640ef80487a20800a0afc77a55c0a0bc91a
Sahar Hamdy <36203865+sahar-hamdy@users.noreply.github.com>
2024-04-17 23:00:53 +0200

    added ProductDesciption class

--- Product_Store_Automation_Project/src/main/java/pages/HomePage.java
+++ Product_Store_Automation_Project/src/main/java/pages/HomePage.java
@@ -1,4 +1,26 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class HomePage {
+    private WebDriver driver;
+    private By choosenProduct= By.xpath("//*[@id='tbodyid']/div[1]/div/div/h4/a");
+
+    public HomePage(WebDriver driver){
+
+        this.driver=driver;
+    }
+
+public ProductDescription chooseItem(){
+    WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(30));
+    wait.until(ExpectedConditions.elementToBeClickable(choosenProduct));
+    driver.findElement(choosenProduct).click();
+return new ProductDescription(driver);
+}
 }
--- Product_Store_Automation_Project/src/test/java/tests/ProductTest.java
+++ Product_Store_Automation_Project/src/test/java/tests/ProductTest.java
@@ -1,4 +1,18 @@
 package tests;
 
-public class ProductTest {
+import org.testng.annotations.Test;
+import base.Base;
+import pages.*;
+import pages.HomePage;
+import pages.ProductDescription;
+import static org.testng.AssertJUnit.assertTrue;
+
+public class ProductTest extends Base  {
+    private ProductDescription productDescriptionPage;
+    @Test
+    public void testGetAddToCart() {
+        productDescriptionPage=homePage.chooseItem();
+        productDescriptionPage.getAddToCart();
+
+    }
 }
